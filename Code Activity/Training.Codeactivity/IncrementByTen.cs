9d40e2e2e64b16732c3439e677d978d1a6a97462
Anusha Khalil <akhalil@students.uit.edu>
2022-09-19 22:27:26 +0500

    Code Activity

---
+++ Code Activity/Training.Codeactivity/IncrementByTen.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training.Codeactivity
+{
+    public class IncrementByTen : CodeActivity
+    {
+        [Input("Number")]
+        public InArgument<int> number { get; set; }
+
+        [Output("result")]
+        public OutArgument<int> result { get; set; }
+
+        ITracingService tracingService;
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            //Create the tracing service
+            ITracingService tracingService = executionContext.GetExtension<ITracingService>();
+
+            //Create the context
+            IWorkflowContext context = executionContext.GetExtension<IWorkflowContext>();
+            IOrganizationServiceFactory serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            try
+            {
+                int number = this.number.Get(executionContext);
+                int incrementedNumber = ((int)(number - (number * (0.1))));
+                tracingService.Trace("workflow output {0}", incrementedNumber);
+                result.Set(executionContext, incrementedNumber);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
